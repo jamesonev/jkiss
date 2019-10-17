3d71e60fececdb083331dfc4c325f9979fdfa486
Jameson Evans <6jh7ln@gmail.com>
2019-10-16 19:51:42 -0500

    jkiss works

--- jkiss.scm
+++ jkiss.scm
@@ -1,38 +1,23 @@
 ; scheme doesn't limit number size so we need a custom function to throw away bits
-; or we will run out of memory. I've chosen to bound at the C ULLONG_MAX standard
+; or we will run out of memory. I've chosen to bound at the C++ ULLONG_MAX minimum standard
 ; (2^64 - 1)
 (define ULLONG_MAX 18446744073709551615) ; (2^64 - 1)
 
 
 (define (expt-mod base pow)
     ; mod any number by ULLONG_MAX
-    (define (mod x) 
+    (define (mod x)
         (modulo x ULLONG_MAX)
     )
-    ; find the largest power of 2 n such that 2^n < pow and the base 
-    (define (find-largest-power current pow)
+    ; core of the expt-mod funciton, O(lg n)
+    (define (recur base product pow)
         (cond
-            ((>= (expt base current) ULLONG_MAX) current)
-            ((<= (* current 2) pow) (find-largest-power (* current 2) pow))
-            (else current)
+            ((<= pow 1) (mod (* base product)) )
+            ((even? pow) (recur (mod (expt base 2)) product (/ pow 2)) )
+            ((odd? pow) (recur base (mod (* base product)) (- pow 1)) )
         )
     )
-    (define (helper base product pow )
-        (define current (find-largest-power 1 pow))
-        (cond
-            ; must be tail recursive or we will reach the recursive call limit for large values of pow
-            ((> pow 1) (helper base (mod (expt base current)) (- pow current) ))
-            ; check that we actually raised base to a power
-            ((and (= pow 1) (= product 1)) (mod base))
-            ; since we know we raised the base to a power,
-            ((= pow 1) (modulo product ULLONG_MAX))
-            ;((= pow 0) 1)
-            (else #f)   ;negatives not implemented
-            )
-        )
-    (helper base 1 pow)
-    ;(find-largest-power 1 pow)
-    ;(expt x 524288)
+    (recur base 1 pow)
 )
 
 (define (jkiss)
@@ -43,31 +28,16 @@
 
     (set! x (* 314527869 (+ x 1234567)))
 
-    (set! y (expt-mod y 100 ULLONG_MAX))
-    ;(set! y (expt-mod y (arithmetic-shift y -7) ULLONG_MAX))
-    ;(set! y (expt-mod y (arithmetic-shift y 22) ULLONG_MAX))
+    (set! y (expt-mod y 5))
+    (set! y (expt-mod y (arithmetic-shift y -7)))
+    (set! y (expt-mod y (arithmetic-shift y 22)))
 
-    ;(define t (* 4294584393ULL (+ z c)))
-    ;(set! c (arithmetic-shift t -32))
-    ;(set! z t)
+    (define t (* 4294584393 (+ z c)))
+    (set! c (arithmetic-shift t -32))
+    (set! z t)
     (+ x y z)
 )
-(define (powmod x pow mod)
-    (define (helper x product pow)
-        (cond
-            ((> pow 1) (helper x (modulo (* x product) mod) (- pow 1)))
-            (else product)
-        )
-    )
-    (helper x 1 mod)
-)
+
 (begin
-    ;(display (expt 987654322 8200))
-    (expt-mod 10433013 133)
-    ;(powmod 4321 12 10000)
-    ;(jkiss)
-    ;(define y 1002400300003294855)
-    ;(define pow (arithmetic-shift y 5))
-    ;(display pow)
-    ;(expt 2 100000)
+    (jkiss)
 )
